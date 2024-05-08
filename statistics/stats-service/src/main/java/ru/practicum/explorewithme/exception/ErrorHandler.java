192cf8186049cb0152df3d33fa4cd65eb11e16b5
Khanbikov-Askhat <Askhat.Khanbikov@Avon.com>
2024-05-08 05:05:55 +0300

    stat_svc

--- statistics/stats-service/src/main/java/ru/practicum/explorewithme/exception/ErrorHandler.java
+++ statistics/stats-service/src/main/java/ru/practicum/explorewithme/exception/ErrorHandler.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.explorewithme.exception.dto.ErrorResponse;
 
-import java.util.Map;
 
 @RestControllerAdvice
 public class ErrorHandler {
