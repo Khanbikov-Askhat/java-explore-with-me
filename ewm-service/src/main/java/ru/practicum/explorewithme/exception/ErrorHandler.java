4ce336ac4c91c2654231dcc78951eeb58185e314
Khanbikov-Askhat <Askhat.Khanbikov@Avon.com>
2024-05-10 11:35:43 +0300

    main_svc

--- ewm-service/src/main/java/ru/practicum/explorewithme/exception/ErrorHandler.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/exception/ErrorHandler.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.practicum.explorewithme.exception.dto.ErrorResponse;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -21,133 +20,133 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ApiError handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.error("Ошибка валидации параметра");
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.BAD_REQUEST, "Ошибка валидации параметра", e.getMessage(),
+        return new ApiError(HttpStatus.BAD_REQUEST, "Ошибка валидации параметра", e.getMessage(),
                 Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleDataValidationException(DataValidationException e) {
+    public ApiError handleDataValidationException(DataValidationException e) {
         log.error("Ошибка в поступивших данных", e);
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.BAD_REQUEST, "Ошибка в поступивших данных", e.getMessage(),
+        return new ApiError(HttpStatus.BAD_REQUEST, "Ошибка в поступивших данных", e.getMessage(),
                 Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+    public ApiError handleDataIntegrityViolationException(DataIntegrityViolationException e) {
         log.error("Нарушено ограничение целостности базы данных");
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.CONFLICT, "Конфликт во время выполнения операции с поступившими данными",
+        return new ApiError(HttpStatus.CONFLICT, "Конфликт во время выполнения операции с поступившими данными",
                 e.getMessage(), Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handleDataConflictException(DataConflictException e) {
+    public ApiError handleDataConflictException(DataConflictException e) {
         log.error("Конфликт при выполнении запроса");
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.CONFLICT, "Конфликт во время выполнения операции с поступившими данными",
+        return new ApiError(HttpStatus.CONFLICT, "Конфликт во время выполнения операции с поступившими данными",
                 e.getMessage(), Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleCategoryNotFoundException(CategoryNotFoundException e) {
+    public ApiError handleCategoryNotFoundException(CategoryNotFoundException e) {
         log.error("Ошибка поиска категории", e);
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.NOT_FOUND, "Ошибка поиска категории", e.getMessage(),
+        return new ApiError(HttpStatus.NOT_FOUND, "Ошибка поиска категории", e.getMessage(),
                 Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleUserNotFoundException(UserNotFoundException e) {
+    public ApiError handleUserNotFoundException(UserNotFoundException e) {
         log.error("Ошибка поиска пользователя");
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.NOT_FOUND, "Ошибка поиска пользователя", e.getMessage(),
+        return new ApiError(HttpStatus.NOT_FOUND, "Ошибка поиска пользователя", e.getMessage(),
                 Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleEventNotFoundException(EventNotFoundException e) {
+    public ApiError handleEventNotFoundException(EventNotFoundException e) {
         log.error("Ошибка поиска события");
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.NOT_FOUND, "Ошибка поиска события", e.getMessage(),
+        return new ApiError(HttpStatus.NOT_FOUND, "Ошибка поиска события", e.getMessage(),
                 Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleCompilationNotFoundException(CompilationNotFoundException e) {
+    public ApiError handleCompilationNotFoundException(CompilationNotFoundException e) {
         log.error("Ошибка поиска подборки");
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.NOT_FOUND, "Ошибка поиска подборки", e.getMessage(),
+        return new ApiError(HttpStatus.NOT_FOUND, "Ошибка поиска подборки", e.getMessage(),
                 Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleRequestNotFoundException(RequestNotFoundException e) {
+    public ApiError handleRequestNotFoundException(RequestNotFoundException e) {
         log.error("Ошибка поиска запроса на участие");
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.NOT_FOUND, "Ошибка поиска запроса на участие", e.getMessage(),
+        return new ApiError(HttpStatus.NOT_FOUND, "Ошибка поиска запроса на участие", e.getMessage(),
                 Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+    public ApiError handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
         log.error("Отстутсвует необходимый параметр запроса");
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.BAD_REQUEST, "Отстутсвует необходимый параметр запроса", e.getMessage(),
+        return new ApiError(HttpStatus.BAD_REQUEST, "Отстутсвует необходимый параметр запроса", e.getMessage(),
                 Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleInvalidRequestException(InvalidRequestException e) {
+    public ApiError handleInvalidRequestException(InvalidRequestException e) {
         log.error("Некорректный запрос");
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.BAD_REQUEST, "Некорректный запрос", e.getMessage(),
+        return new ApiError(HttpStatus.BAD_REQUEST, "Некорректный запрос", e.getMessage(),
                 Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleUnknownException(Throwable e) {
+    public ApiError handleUnknownException(Throwable e) {
         log.error("Возникла непредвиденная ошибка", e);
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         String stackTrace = out.toString();
-        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Непредвиденная ошибка", e.getMessage(),
+        return new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Непредвиденная ошибка", e.getMessage(),
                 Collections.singletonList(stackTrace), LocalDateTime.now());
     }
 }
