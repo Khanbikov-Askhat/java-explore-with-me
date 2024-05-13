c2696e378a9cedf4c83074c769ea07fa9202f1e7
Khanbikov-Askhat <Askhat.Khanbikov@Avon.com>
2024-05-13 09:39:27 +0300

    added logging

--- ewm-service/src/main/java/ru/practicum/explorewithme/user/controller/UserPrivateController.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/user/controller/UserPrivateController.java
@@ -1,6 +1,7 @@
 package ru.practicum.explorewithme.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import ru.practicum.explorewithme.user.service.UserService;
 @Validated
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserPrivateController {
 
     private final UserService userService;
@@ -17,12 +19,17 @@ public class UserPrivateController {
     @PostMapping(value = "/users/{userId}/followers/{followerId}")
     @ResponseStatus(HttpStatus.CREATED)
     public UserWithFollowersDto addFollower(@PathVariable Long userId, @PathVariable Long followerId) {
-        return userService.addFollower(userId, followerId);
+        log.info("Получен запрос POST /users/{}/followers/{}", userId, followerId);
+        UserWithFollowersDto user = userService.addFollower(userId, followerId);
+        log.info("Добавлен подписчик: {}", user);
+        return user;
     }
 
     @DeleteMapping(value = "/users/{userId}/followers/{followerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteFollower(@PathVariable Long userId, @PathVariable Long followerId) {
+        log.info("Получен запрос DELETE /users/{}/followers/{}", userId, followerId);
+        log.info("Удален подписчик: {}", userId);
         userService.deleteFollower(userId, followerId);
     }
 }
